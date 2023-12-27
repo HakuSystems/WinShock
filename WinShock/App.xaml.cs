c3fd0e30fd1b1aaf5f325de228afb950d0fd5e21
lyze <66133638+HakuSystems@users.noreply.github.com>
2023-12-27 13:23:06 +0100

    Add global exception handling and WPF-UI package
    This commit introduces a global exception handler in App.xaml.cs to catch unhandled exceptions and notify the users with a MessageBox. In addition, a new WPF-UI package is included in the WinShock.csproj for enhancing the user interface.

--- WinShock/App.xaml.cs
+++ WinShock/App.xaml.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Threading;
 
-namespace WinShock
+namespace WinShock;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public App()
     {
+        Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+    }
+
+    private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        e.Handled = true;
     }
 }
